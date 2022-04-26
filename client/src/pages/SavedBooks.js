b8bf46f60fd07f581bda488d0a7cd65b2f65f464
NGGKI <kien.dv2012@gmail.com>
2022-04-25 22:02:49 -0700

    fix bugs

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import {
   Jumbotron,
   Container,
   CardColumns,
   Card,
   Button,
-} from "react-bootstrap";
+} from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
@@ -22,7 +22,7 @@ const SavedBooks = () => {
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-
+    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -41,7 +41,6 @@ const SavedBooks = () => {
     }
   };
 
-  // if data isn't here yet, say so
   if (loading) {
     return <h2>LOADING...</h2>;
   }
@@ -50,18 +49,18 @@ const SavedBooks = () => {
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing saved books!</h1>
+          <h1>Viewing {userData.username}'s books!</h1>
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? "book" : "books"
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'
             }:`
-            : "You have no saved books!"}
+            : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border="dark">
                 {book.image ? (
